28559592b762b2f8be9e3f30342cb32c3f8e779a
nndakurnia <nandaka38@gmail.com>
2024-03-21 21:05:59 +0700

    rebuild footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,32 @@ module.exports = {
         primary: '#FA4A0C',
         dark: '#252B42',
       },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'roboto': ['Roboto', 'sans-serif'],
+      },
+      fontSize: {
+        'h2': ['50px', {
+          lineHeight: '57px',
+          letterSpacing: '0.2px',
+          fontWeight: 'bold',
+        }],
+        'h4': ['24px', {
+          lineHeight: '38px',
+          letterSpacing: '0.2px',
+          fontWeight: '500',
+        }],
+        'button': ['16px', {
+          lineHeight: '28px',
+          letterSpacing: '0.2px',
+          fontWeight: '700',
+        }],
+        'subtitle': ['12px', {
+          lineHeight: '18px',
+          letterSpacing: '0.1px',
+          fontWeight: '700',
+        }],
+      }
     },
   },
   plugins: [],
